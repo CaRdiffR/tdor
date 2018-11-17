cacd83a1ad92f1a04057ba0f4a47d041c90a8bac
Dave Parr <DavidRGParr@gmail.com>
2018-11-17 15:59:26 +0000

    update scirpt to 'validate' good records as ones with permalinks

--- data-raw/tdor.R
+++ data-raw/tdor.R
@@ -4,13 +4,15 @@ library("janitor")
 # Get Data ----
 # Data sourced from https://bitbucket.org/annajayne/tdor_data/downloads/, unzipped and the contents of the data folder placed in data-raw
 
-map(list.files)
-# Read as list of tibbles
 list.files("./data-raw/tdor_data",
            "*.csv",
            recursive = TRUE,
-           full.names = TRUE)[1:12]  %>%
+           full.names = TRUE)  %>%
   map( ~ read_csv(.)) %>%
   map(., mutate, Age = as.character(Age)) %>%
   bind_rows() %>%
-  remove_empty() -> tdor_files
+  remove_empty(dat = ., which = c("rows", "cols")) %>%
+  filter(!is.na(Permalink))-> tdor_files
+
+
+
