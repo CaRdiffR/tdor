f73113f40604fece7670cccda65987208c6133d6
Heather Turner <ht@heatherturner.net>
2019-11-18 14:00:09 +0000

    add TDoR period

--- R/tdor.R
+++ R/tdor.R
@@ -1,9 +1,33 @@
 #' Transgender Day of Remembrance Data
 #'
 #' This data set contains information on killings and suicides of transgender
-#' people.
+#' people, collected for the Transgender Day of Remembrance.
 #'
-#' @format A data frame with 2705 records and 21 variables:
+#' The Transgender Day of Remembrance (TDoR) is held annually on November 20.
+#' On this day, people memorialise those who have been murdered or lost to
+#' suicide as a result of transphobia in the previous year.
+#'
+#' The `TDoR` variable notes the particular TDoR that a record relates to. The
+#' first TDoR was held in 1999, so `TDoR` is missing before 1998-11-19. Until
+#' TDoR 2010, each TDoR memorialised deaths that had occurred since the last
+#' TDoR. From 2009, the Trans Murder Monitoring project from
+#' Transgender Europe (TGEU) began to release annual updates of relevant deaths.
+#' During the years #' 2011 to 2013 the period these updates covered shifted
+#' earlier in the year, to allow time for records to be checked and for
+#' the reports to be made in time for the day of remembrance itself.
+#' In these years the periods were
+#' \describe{
+#'     \item{2011}{November 20 2010 to November 14 2011.}
+#'     \item{2012}{November 15 2011 to November 14 2012.}
+#'     \item{2013}{November 20 2012 to November 1 2013.}
+#' }
+#' From 2014 onwards the period has been from October 1 to September 30. To
+#' avoid ambiguity and allow newly discovered records to be added, the `TDoR`
+#' variable closes the gap between 2012 and 2013, setting the end date to
+#' November 19, and assign deaths from October 1 2013 to November 1 2013 to
+#' TDoR 2014.
+#'
+#' @format A data frame with 3513 records and 19 variables:
 #' \describe{
 #'     \item{Name}{Name (character).}
 #'     \item{Age}{Age (character), typically age in years, but may also be
@@ -11,11 +35,12 @@
 #'     "Approx. 30" or "Under 35").}
 #'     \item{Age_min}{Minium age (numeric) if this can be defined from Age.}
 #'     \item{Age_max}{Maxium age (numeric) if this can be defined from Age.}
-#'     \item{Photo}{Name of photo file in data source (not in package yet).}
-#'     \item{Photo source}{URL where photo obtained from.}
+#'     \item{Photo source}{URL where photo was obtained from for
+#'     tdor.translivesmatter.info website.}
 #'     \item{Date}{Date of death.}
 #'     \item{Month}{Month of death.}
 #'     \item{Year}{Year of death.}
+#'     \item{TDoR}{TDoR period, see Details.}
 #'     \item{Source ref}{Original news source.}
 #'     \item{Location}{Location of death.}
 #'     \item{State/Province}{The state or province. If `NA`, the state/province
--- data-raw/tdor.R
+++ data-raw/tdor.R
@@ -8,9 +8,12 @@ library("usethis")
 
 # Get Data ----
 # Data sourced from https://bitbucket.org/annajayne/tdor_data/downloads/, unzipped and the contents of the data folder placed in data-raw
+dir <- "./data-raw/tdor_data"
+# OR fork/clone https://bitbucket.org/annajayne/tdor_data
+dir <- "../tdor_data/Data"
 
-## data corrsponding to commit: 4ef244b
-files <- list.files("./data-raw/tdor_data",
+## data corresponding to commit: 9174f76
+files <- list.files(dir,
                     "*.csv$",
                     recursive = TRUE,
                     full.names = TRUE)
@@ -27,6 +30,7 @@ nm <- tdor_raw %>%
 unique_nm <- nm %>% unlist() %>% unique()
 empty <- grep("^[X][0-9]+$", unique_nm)
 actual_nm <- unique_nm[-empty]
+actual_nm
 
 # Consider fixing and submitting PR to Bitbucket repo, otherwise allow for variants below
 
@@ -39,24 +43,30 @@ col_types <- cols_only(
   Date = col_character(),
   `Source ref` = col_character(),
   Location = col_character(),
-  `State/Province` = col_character(),
   Country = col_character(),
   Latitude = col_double(),
   Longitude = col_double(),
   `Cause of death` = col_character(),
   Description = col_character(),
-  Tweet = col_character(),
-  Permalink = col_character()
+  Permalink = col_character(),
+  `State/Province` = col_character(),
+  Tweet = col_character()
 )
+
+# combine all data files
 
 tdor <- files %>%
   map(~ suppressWarnings(read_csv(., col_types = col_types))) %>%
   # combine and remove redundant rows
   bind_rows() %>%
   remove_empty(which = "rows") %>%
-  select(!!names(col_types$cols)) %>%
-  # fix known data issues
-  filter(!is.na(Name)) %>% # empty row with one "`" in cause of death column
+  select(!!names(col_types$cols))
+
+# fix known data issues ... currently all fixed at source
+
+# add R only variables/fix up data types
+
+tdor <- tdor %>%
   # sometimes age is a range; separate into min and max
   separate(Age, into = c("Age_min", "Age_max"), sep = "-", remove = FALSE) %>%
   mutate(Age_min = suppressWarnings(ifelse(!is.na(as.numeric(Age_min)),
@@ -69,8 +79,25 @@ tdor <- files %>%
   # simple conversion to TDOR period no longer works
   mutate(Date = dmy(Date),
          Month = month(Date),
-         Year = year(Date)) %>%
-  select(Name:Date, Month, Year, everything())
+         Year = year(Date),
+         TDoR =
+           case_when(
+             # TDoR started in 1999
+             Date < as.Date("1998-11-19") ~ NA_real_,
+             # up to 2010, covered year in between TDoR days
+             between(Date, as.Date("1998-11-20"), as.Date("2010-11-19")) ~
+               case_when(Month %in% 1:10 ~ Year,
+                         Month == 12 ~ Year  + 1,
+                         day(Date) < 20 ~ Year,
+                         TRUE ~ Year + 1),
+             # for a few years cutoff date kept shifting earlier
+             Date < as.Date("2011-11-14") ~ 2011,
+             Date < as.Date("2012-11-19") ~ 2012,
+             Date < as.Date("2013-09-30") ~ 2013,
+             # 1st October to 30th September from now on
+             TRUE ~ ifelse(Month %in% 1:9, Year, Year + 1)
+           )) %>%
+  select(Name:Date, Month, Year, TDoR, everything(), -Photo)
 
 use_data(tdor, overwrite = TRUE)
 
